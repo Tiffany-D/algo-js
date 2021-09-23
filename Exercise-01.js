6f0769b0a8ec3137a05c9b47112b6c625c72d719
Tiffany Dessouroux <tiffany.dessouroux@gmail.com>
2021-09-23 14:13:42 +0200

    exercises part 1 finished

---
+++ Exercise-01.js
@@ -0,0 +1,68 @@
+//Exercise 1.0 - Interactive Mode
+
+//Launch node in interactive mode and use it to make an addition. Then repeat the same step until you have tested all five arithmetic operators we presented before.
+
+// Interactive mode
+
+// Launch node in Interactve Mode -> "node" in terminal.
+//REPL stands for read-eval-print loop and basically means command line. To use it, you must first start Node.js from an operating system command line,
+//via the command node.  
+
+
+
+//Exercise 1.1
+
+//Define a variable and display it in the console.
+
+//let name = "Tiffany";
+//console.log(name);
+
+//Exercise 1.2
+
+//Define three variables: name, firstName and city. Display them like this: "Your name is Gerard Lambert and you live in Paris.".
+
+//let lastName = "Dessouroux";
+//let firstName = "Tiffany";
+//let city = "Liège";
+//console.log("My name is" + firstName + lastName ,"and I live in" + city);
+
+
+
+//Exercise 1.3
+
+//Ask the user to enter its first name and display a message saying "Hello first name".
+
+//let userName = readlineSync.question("What's your name?");
+//console.log("Hello " + userName);
+
+//Exercise 1.4
+
+//Make this same thing as Exercise 2 but this time ask the user for the name, the first name and the city.
+//const readlineSync = require("readline-sync");
+//let userName = readlineSync.question("What's your first name?");
+//let lastName = readlineSync.question("What's your last name?");
+//let city = readlineSync.question("Where do you live?");
+//console.log("You're " + userName + lastName, "and you live in " + city);
+
+
+//Exercise 1.5
+
+//Ask two numbers with decimal part to the user. For the first one only keep the integer part. Then multiply them and display the result.
+
+//You will have to perform a search on Google to know how to only keep the integer part of a number.
+//const readlineSync = require("readline-sync");
+//let decimalNumberOne = readlineSync.question("Give a number with a decimal");
+//let decimalNumberTwo = readlineSync.question("Give another number with a decimal");
+
+//console.log(Math.round(decimalNumberOne) * decimalNumberTwo);
+
+//Exercise 1.6
+
+//Ask two integers to the user. Display the rest of the integer division of the two numbers.
+
+//const readlineSync = require("readline-sync");
+//let integerOne = readlineSync.question("Can you give me one integer ?");
+//let integerTwo = readlineSync.question("Can you give me another integer ?");
+//console.log(Math.floor(integerOne) / Math.floor(integerTwo));
+
+
--- test.js
+++ test.js
@@ -1,10 +1,54 @@
-const readlineSync = require("readline-sync");
-let userName = readlineSync.question('Can you give me your name please?');
-console.log("Hello " + userName);
+//const readlineSync = require("readline-sync");
+//let userName = readlineSync.question('Can you give me your name please?');
+//console.log("Hello " + userName);
 
 
 
-console.log("It works !!!");
+//console.log("It works !!!");
 
 
+// Numbers
+//let age = 25;
 
+//Numbers 2
+//let age = 23;
+
+//let sisterAge = age + 11;
+
+//age += 12; // age = age + 12;
+
+//console.log(age);
+
+
+//Strings
+//let phrase = "Coding is fun!";
+
+//Strings 2
+//let name = "Jean";
+//let nameSister = "Emma";
+//console.log("My name is " + name);
+
+//let userName = readlineSync.question("What's your name?");
+//console.log("He's called " + userName);
+
+//New Number ()
+//let htmlBadges = new Number(readlineSync.question('How many HTML badges do you have?'));
+
+//let cssBadges = new Number(readlineSync.question('How many CSS badges do you have?'));
+
+//let totalBadges = htmlBadges + cssBadges;
+
+//console.log('Woaw, you have ' + totalBadges + "!");
+
+//new Number() converts a string to a number.
+
+// Booleans
+//let isItTrue = true;
+//let isItFalse = false;
+
+// Beware of variable types
+//console.log('10' + '5'); // '105' => string type
+
+//console.log(10 + 5); // 15 => number type
+
+//--> Explanations
