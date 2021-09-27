ff09f087e1cf96c3c8b7509e9a536e336c93388b
Tiffany Dessouroux <tiffany.dessouroux@gmail.com>
2021-09-27 16:59:17 +0200

    in progress

---
+++ Exercise-06.js
@@ -0,0 +1,16 @@
+//Exercise 6.1
+
+//Create a class named Circle with attributes xPos, yPos and radius. Add a method named move(xOffset, yOffset) that will adjust the position of the circle. Add a getter accessor named surface that will return the surface of the circle.
+
+//Test its method and accessors by modifying the values and checking if everything is consistent.
+
+class Circle{
+    constructor(xPos, yPos, radius) {
+        this.xOffset = xPos;
+        this.yOffset = yPos;
+        this.radius = radius;
+    }
+    get surface() {
+        
+    }
+}
